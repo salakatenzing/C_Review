acb49bede3c1b9bb90556f74878ce693c8e445d2
Tenzing Salaka <salakatenzing@gmail.com>
2023-07-16 12:47:08 -0400

    initial

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+        
+	int p_value = 501;
+	char s_value[] = "S_Add";
+	printf("Value: %i \n", p_value);
+	printf("s_value: %s \n", s_value);
+
+	return 0;
+
+}
