2ddfe196352e0dde6e63f2b41c3dd62d5c00e056
Tenzing Salaka <salakatenzing@gmail.com>
2023-07-16 13:04:09 -0400

    arrays

--- main.c
+++ main.c
@@ -5,9 +5,17 @@ int main() {
         
 	int p_value = 501;
 	char s_value[] = "S_Add";
+	float weight = 54.452;
 	printf("Value: %i \n", p_value);
 	printf("s_value: %s \n", s_value);
+	printf("weight value: %.2f \n", weight);
 
+
+	int size_val[] = {1, 2, 3, 4, 5};
+	int size = sizeof(size_val);
+	int array_length = sizeof(size_val)/sizeof(size_val[0]);
+	printf("size of array in bytes: %d \n", size);
+	printf("length of array: %d \n", array_length);
 	return 0;
 
 }
